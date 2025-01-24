eee543c9aa18a4041466a4781bc167654148228b
m-radkova <masha_radkova@mail.ru>
2025-01-25 00:24:56 +0300

    added allure

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,4 +1,5 @@
 import random
+import allure
 import pytest
 from time import sleep
 from pages.elements_page import TextBoxPage
@@ -6,8 +7,11 @@ from pages.elements_page import RadioButtonPage
 from pages.elements_page import WebTablesPage
 
 
+@allure.suite("Elements")
+@allure.feature("Text Box")
 class TestTextBox:
 
+    @allure.title("Check Text Box")
     def test_text_box(self, text_box_page):
         text_box_page.open_page()
         name, email, current_address, permanent_address = text_box_page.fill_all_fields_and_submit()
@@ -18,8 +22,9 @@ class TestTextBox:
         assert permanent_address == output_per_addr, "permanent address does not match"
 
 
+@allure.feature("Check Box")
 class TestCheckBox:
-
+    @allure.title("Check Check Box")
     def test_check_box(self, check_box_page):
         check_box_page.open_page()
         check_box_page.expand_all()
@@ -29,8 +34,9 @@ class TestCheckBox:
         assert input_list == output_list, "the output text does not match the input checkboxes"
 
 
+@allure.feature("Radio Button")
 class TestRadioButton:
-
+    @allure.title("Check Radio Button")
     def test_radio_button(self, radio_button_page):
         choice1, choice2, choice3 = 'Yes', 'Impressive', 'No'
         radio_button_page.open_page()
@@ -45,23 +51,24 @@ class TestRadioButton:
         assert output_3 == choice3, f'"{choice3}" has not been selected or choice and output text does not match'
 
 
+@allure.feature("Web Table")
 class TestWebTables:
-
+    @allure.title("Add person to web table")
     def test_web_table_add_person(self, web_tables_page):
         web_tables_page.open_page()
         web_tables_page.add_person()
-        sleep(5)
 
+    @allure.title("Check new person in web table")
     def test_web_table_check_new_person(self, web_tables_page):
         web_tables_page.open_page()
         new_person = web_tables_page.add_person()
         result = web_tables_page.check_new_person()
         assert new_person in result, "new person not found in the table"
 
-
+    @allure.title("Check search person")
     def test_web_table_search_person(self, web_tables_page):
         web_tables_page.open_page()
-        key_word = web_tables_page.add_person()[random.randint(0,5)]
+        key_word = web_tables_page.add_person()[0]
         web_tables_page.search_person(key_word)
         result = web_tables_page.check_search_person()
         print(key_word)
